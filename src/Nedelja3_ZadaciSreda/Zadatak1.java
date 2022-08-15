77be7137b9f0db870378d482495dc2333b8067bc
IvicVladica <ivicvladica@gmail.com>
2022-08-15 19:47:40 +0200

    Test

---
+++ src/Nedelja3_ZadaciSreda/Zadatak1.java
@@ -0,0 +1,35 @@
+package Nedelja3_ZadaciSreda;//1. //Napi²ite Java program za brojanje stringova u kojima je duºina
+//       stringa 2 ili vi²e, a prvi i poslednji znak isti sa date liste stringova.
+//Stringove uneti sa tastature pomo¢u skenera i unabici u
+//       ArrayList strukturu podataka
+// Potrebno je napraviti funkciju
+//ArrayList : ['abc', 'xyz', 'aba', '1221']
+//Expected Result : 2
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Zadatak1 {
+    public static int ArrayList(String []arr){
+        int count = 0;
+        for (String element : arr){
+            if (element.charAt(0) == element.charAt(element.length()-1) && element.length()>2) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Unesite clanove niza: ");
+        String input = sc.nextLine();
+        String [] input1 = input.split(", ");
+        String niz [] = new String [input1.length];
+        for (int i=0; i<input1.length; i++) {
+            niz [i] = input1[i];
+        }
+        System.out.println(Arrays.toString(niz));
+        System.out.println(ArrayList(niz));
+    }
+}
---
+++ src/Nedelja3_ZadaciSreda/Zadatak2.java
@@ -0,0 +1,28 @@
+package Nedelja3_ZadaciSreda;//2. //Napisite Java funkciju koja uzima dva niza i vraca True ako
+//        imaju bar jednog zajednickog clana
+
+public class Zadatak2 {
+    public static boolean contains (String arr[],String x){
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i].equals(x)){
+                return true;
+        }
+        }
+        return false;
+    }
+
+    public static boolean checkArrays (String arr1[], String arr2[]) {
+        for (int i=0; i<arr1.length; i++){
+            if (contains(arr2, arr1[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String a [] = {"1","2","3","4","5"};
+        String b [] = {"1","8","8","8","8","8"};
+        System.out.println(checkArrays(a,b));
+    }
+}

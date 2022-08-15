951d55a512f4ed2888c4a61ff3527e96cabedcf7
IvicVladica <111304308+IvicVladica@users.noreply.github.com>
2022-08-15 20:24:52 +0200

    Add files via upload

---
+++ Nedelja3_ZadaciNedelja/Zadatak1.java
@@ -0,0 +1,42 @@
+package Nedelja3_ZadaciNedelja;//1. Написати функцију која прима матрицу (2Д низ) и враћа највећи ред (по збиру)
+//        Hint: Сетите се како смо радили највећи елемент низа
+//        int[][] matrix = { { 3, 2, 1, 7 },
+//        { 9, 11, 5, 4 },
+//        { 6, 0, 13, 17 },
+//        { 7, 21, 14, 15 } };
+//        largestRow(matrix); // Враћа - [7, 21, 14, 15]
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Zadatak1 {
+    public static int[] largestRow (int[][]input){
+        int sumRow = 0;
+        int sumMax = 0;
+        int n=input[0].length;
+        int[] result = new int[n];
+        for (int [] row : input) {
+            sumRow = 0;
+            for (int el : row){
+                sumRow = sumRow + el;
+            }
+            if (sumRow>sumMax) {
+                sumMax=sumRow;
+                result = row;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 3, 2, 1, 7 },
+                           { 9, 11, 5, 4 },
+                           { 6, 0, 13, 17 },
+                           { 7, 21, 14, 15 } };
+
+
+        System.out.println(Arrays.toString(largestRow(matrix)));
+
+    }
+}
